3f3a3fdb00175cbda0cf020231e172ebb0c4145b
Markyis <95830260+Markyis@users.noreply.github.com>
2022-03-26 17:47:32 +0800

    Add files via upload

---
+++ assets/preload/data/sporting/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark',   songName    .. " | Port by Walsubaks", 0, 22, 640); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
